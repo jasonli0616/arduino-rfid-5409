aadcd7dc876475ab0d21fed8ae250e9c5188ff95
Jason Li <jasonli.0616@outlook.com>
2024-02-04 12:34:28 -0500

    Read NFC card from Arduino

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <Adafruit_PN532.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PN532_IRQ 2
+#define PN532_RESET 2
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+
+  // Set up reader
+  nfc.begin();
+  if (!nfc.getFirmwareVersion()) {
+    Serial.println("ERROR: Didn't find PN532 board.");
+    while (true) {} // Stop program
+  }
+
+  Serial.println("READY");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+
+    // Print UID
+    Serial.print("SUCCESS: ");
+    for (uint8_t i = 0; i < uidLength; i++) {
+      Serial.print(uid[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+
+    // Delay before reading next card
+    delay(1000);
+  }
+
 }
